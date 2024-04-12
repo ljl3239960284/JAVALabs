c28dd16a0ab5704ccf9d9c31240746441685ee08
worsion PC <56216974+ljl3239960284@users.noreply.github.com>
2024-04-12 17:59:35 +0800

    Lab6

---
+++ Ideaproject/javabasic/Hacker/src/homework6/MainTest.java
@@ -0,0 +1,20 @@
+package homework6;
+
+public class MainTest {
+
+}
+class X{
+    public void method(int i) {
+        System.out.println("one");
+    }
+    public void method(double d) {
+        System.out.println("two");
+    }
+}
+
+
+class Y extends X{
+    public void method(double d){
+
+    }
+}
