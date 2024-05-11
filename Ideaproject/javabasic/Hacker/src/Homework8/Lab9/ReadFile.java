92b117e8b8912aa0f542c16ff898604ef8a73e24
worsion PC <56216974+ljl3239960284@users.noreply.github.com>
2024-05-11 17:46:24 +0800

    File

---
+++ Ideaproject/javabasic/Hacker/src/Homework8/Lab9/ReadFile.java
@@ -0,0 +1,39 @@
+package Homework8.Lab9;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        System.out.print("Enter the directory address: ");
+        Scanner sc = new Scanner(System.in);
+
+        String address;
+        address = sc.nextLine();
+
+        File currentdirectory = new File(address);
+
+        String[] filelist = currentdirectory.list();
+
+        if (filelist!= null) {
+            System.out.println("Files in current directory: ");
+            System.out.println(Arrays.toString(filelist));
+        }else{
+            System.out.println("No files in current directory");
+        }
+        
+        try(BufferedReader fr = new BufferedReader(address)){
+            String line;
+            while ((line = fr.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch(IOException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+}
